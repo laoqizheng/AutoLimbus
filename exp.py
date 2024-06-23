a42e22e69c8b191b881b5757ae7fdd4cefbb2107
laoqizheng <1330172898@qq.com>
2024-06-23 11:23:16 +0800

    经验本优化

--- exp.py
+++ exp.py
@@ -5,41 +5,52 @@ import pyautogui
 import cv2
 import numpy as np
 
+drive_pic = cv2.imread('./pic/drive.png')
+exp_logo_pic = cv2.imread('./pic/exp/exp_logo.png')
+exp_6_pic = cv2.imread('./pic/exp/exp_6.png')
+join_pic = cv2.imread('./pic/exp/join.png')
+join_num_pic = cv2.imread('./pic/exp/join_num.png')
+win_rate = cv2.imread('./pic/win_rate.png')
+win_pic = cv2.imread('./pic/exp/win.png')
+lose_pic = cv2.imread('./pic/exp/lose.png')
+tag1_pic = cv2.imread('pic/exp/tag1.png')
+tag2_pic = cv2.imread('pic/exp/tag2.png')
+
+
 # 经验本
 def go_exp(num = 1):
     start_time = time.time()
     current = 1
+    current_tag = False
+    fun.print_module()
     while current <= num:
-        screenshot = pyautogui.screenshot()
-        template = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-        drive_pic = cv2.imread('./pic/drive.png')
-        exp_logo_pic = cv2.imread('./pic/exp/exp_logo.png')
-        exp_6_pic = cv2.imread('./pic/exp/exp_6.png')
-        if fun.find(template, exp_6_pic)[0] != False:
-            exp_6 = cv2.imread('./pic/exp/exp_6.png')
-            loc1, loc2 = fun.find(template, exp_6)
+        template = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_BGR2RGB)
+        if fun.find(template, exp_6_pic)[0]:
+            loc1, loc2 = fun.find(template, exp_6_pic)
             screenshot_exp_6 = pyautogui.screenshot(region=[loc1[0], loc1[1], loc2[0], loc2[1]])
             template_exp_6 = cv2.cvtColor(np.array(screenshot_exp_6), cv2.COLOR_BGR2RGB)
-            join_pic = cv2.imread('./pic/exp/join.png')
             loc3, loc4 = fun.find(template_exp_6, join_pic)
             fun.simulate_move((loc1[0] + loc3[0], loc1[1] + loc3[1]), (loc1[0] + loc4[0], loc1[1] + loc4[1]))
             fun.randomSleep(500, 1000)
             fun.simulate_click()
             while True:
-                screenshot_ready = pyautogui.screenshot()
-                template_ready = cv2.cvtColor(np.array(screenshot_ready), cv2.COLOR_BGR2RGB)
-                join_num_pic = cv2.imread('./pic/exp/join_num.png')
-                if fun.find(template_ready, join_num_pic)[0] != False:
-                    pyautogui.press('enter')
-                    break
-                else:
-                    fun.sleep(1000)
+                template_ready = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_BGR2RGB)
+                if current_tag:
+                    if fun.find(template_ready, join_num_pic)[0]:
+                        pyautogui.press('enter')
+                        break
+                    else:
+                        fun.sleep(1000)
+                elif fun.find(template_ready, tag1_pic)[0]:
+                    current_tag = True
+                elif fun.find(template_ready, tag2_pic)[0]:
+                    fun.find_and_click(template_ready, tag2_pic)
             print('第', current, '次战斗')
             fight()
             current += 1
-        elif fun.find(template, exp_logo_pic)[0] != False:
+        elif fun.find(template, exp_logo_pic)[0]:
             fun.find_and_click(template, exp_logo_pic)
-        elif fun.find(template, drive_pic)[0] != False:
+        elif fun.find(template, drive_pic)[0]:
             fun.find_and_click(template, drive_pic)
         else:
             fun.sleep(1000)
@@ -48,15 +59,12 @@ def go_exp(num = 1):
     print('战斗场次', num)
     print('总耗时', math.floor(spend_seconds / 60), '分', math.floor(spend_seconds) % 60, '秒')
 
+
 # 战斗
 def fight():
-    win_rate = cv2.imread('./pic/win_rate.png')
-    win_pic = cv2.imread('./pic/exp/win.png')
-    lose_pic = cv2.imread('./pic/exp/lose.png')
     turn = 1
     while True:
-        screenshot = pyautogui.screenshot()
-        template = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
+        template = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_BGR2RGB)
         if fun.find(template, win_rate)[0] != False:
             pyautogui.press('p')
             fun.randomSleep(500, 1000)
@@ -78,8 +86,4 @@ def fight():
             fun.sleep(1000)
 
 
-def get_module():
-    print('模块数：')
-
-
-go_exp(2)
+go_exp(1)
--- fun.py
+++ fun.py
@@ -3,11 +3,14 @@ import cv2
 import time
 import random
 import numpy as np
+import pytesseract
+from PIL import Image
+
 
 # 图像识别
 def find(template, img):
     res = cv2.matchTemplate(template, img, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
+    threshold = 0.9
     loc = np.where(res >= threshold)
     if loc[0].size > 0:
         h, w = img.shape[:2]
@@ -20,26 +23,31 @@ def find(template, img):
     # cv2.imshow('res', cv2.rectangle(screenshot, top_left, bottom_right, (0, 255, 0), 2))
     # cv2.waitKey(0)
 
+
 # 随机休眠
 def randomSleep(min, max):
     sleep_num = random.randint(min, max) / 1000
     # print('休眠时间', sleep_num, '秒')
     time.sleep(sleep_num)
 
+
 # 休眠
 def sleep(num):
     time.sleep(num / 1000)
+
 
 # 模拟鼠标移动
 def simulate_move(tup1, tup2):
     pyautogui.moveTo(random.randint(tup1[0], tup2[0]), random.randint(tup1[1], tup2[1]))
 
+
 # 模拟鼠标点击
 def simulate_click():
     pyautogui.click()
 
+
 # 识别图片并点击
-def find_and_click(template, img, min_sleep = 500, max_sleep = 1000):
+def find_and_click(template, img, min_sleep=500, max_sleep=1000):
     tup1, tup2 = find(template, img)
     if tup1 == False:
         print('第一次识别失败')
@@ -49,11 +57,6 @@ def find_and_click(template, img, min_sleep = 500, max_sleep = 1000):
     randomSleep(min_sleep, max_sleep)
     simulate_click()
 
-# 自动镜牢
-# def auto():
-#     main = cv2.imread('./pic/main.png')
-#     drive = cv2.imread('./pic/drive.png')
-#     find(main, drive)
 
 # 选择ego
 def choose_ego():
@@ -104,135 +107,10 @@ def choose_reward():
             break
 
 
-# 进入战斗
-def join_fight():
-    print('进入')
-    screenshot = pyautogui.screenshot()
-    tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-    join_pic = cv2.imread('./pic/join_fight/join.png')
-    join_people = cv2.imread('./pic/join_fight/join_people.png')
-    randomSleep(300, 1000)
-    find_and_click(tem, join_pic)
-    while True:
-        sleep(1000)
-        screenshot = pyautogui.screenshot()
-        tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-        tup1, tup2 = find(tem, join_people)
-        if tup1 != False:
-            tup1 = (tup1[0], tup1[1] + 180)
-            tup2 = (tup2[0], tup2[1] + 230)
-            simulate_move(tup1, tup2)
-            randomSleep(500, 1000)
-            simulate_click()
-            print('开始战斗')
-            break
-
-
-# 结束镜牢
-def finish():
-    screenshot = pyautogui.screenshot()
-    tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-    confirm_pic = cv2.imread('./pic/finish/confirm.png')
-    get_pic = cv2.imread('./pic/finish/get.png')
-    confirm2_pic = cv2.imread('./pic/finish/confirm2.png')
-    confirm3_pic = cv2.imread('./pic/finish/confirm3.png')
-    confirm4_pic = cv2.imread('./pic/finish/confirm4.png')
-    randomSleep(1000, 2000)
-    find_and_click(tem, confirm_pic)
-    randomSleep(1000, 2000)
-    simulate_click()
-
-    randomSleep(1000, 2000)
-    screenshot = pyautogui.screenshot()
-    tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-    find_and_click(tem, get_pic)
-
-    randomSleep(1000, 2000)
-    screenshot = pyautogui.screenshot()
-    tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-    find_and_click(tem, confirm2_pic)
-
-    randomSleep(2000, 3000)
-    screenshot = pyautogui.screenshot()
-    tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-    find_and_click(tem, confirm3_pic)
-
-    randomSleep(2000, 3000)
-    screenshot = pyautogui.screenshot()
-    tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-    find_and_click(tem, confirm4_pic)
-
-    print('结束镜牢')
-
-# 自动战斗
-def fight():
-    win_rate = cv2.imread('./pic/win_rate.png')
-    go = cv2.imread('./pic/go.png')
-    max = cv2.imread('./pic/max.png')
-    book = cv2.imread('./pic/book.png')
-    get_ego = cv2.imread('./pic/get_ego.png')
-    choose_reward_pic = cv2.imread('./pic/choose_reward/choose_reward.png')
-    win_pic = cv2.imread('./pic/finish/win.png')
-    join_pic = cv2.imread('./pic/join_fight/join.png')
-    card_pic = cv2.imread('./pic/choose_card/card.png')
-
-    turn = 1
-    # 战斗状态
-    fight_status = False
-    # 选择卡包状态
-    choose_card_status = False
-    while True:
-        screenshot = pyautogui.screenshot()
-        tem = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-        res1 = find(tem, book)[0]
-        res2 = find(tem, max)[0]
-        res3 = find(tem, get_ego)[0]
-        res4 = find(tem, choose_reward_pic)[0]
-        res5 = find(tem, win_pic)[0]
-        res6 = find(tem, join_pic)[0]
-        res7 = find(tem, card_pic)[0]
-        # print('res1', res1)
-        # print('res2', res2)
-        # print('res3', res4)
-        # print('res4', res4)
-        # print('res5', res5)
-        # print('res6', res6)
-        if res6 != False:
-            join_fight()
-        elif res1 != False:
-            continue
-        elif res2 != False:
-            fight_status = True
-            find_and_click(tem, win_rate)
-            randomSleep(300, 1000)
-            find_and_click(tem, go)
-            print('回合', turn)
-            turn += 1
-            sleep(4000)
-        elif res3 != False:
-            if fight_status == True:
-                fight_status = False
-                turn = 1
-                print('战斗结束')
-            choose_ego()
-        elif res4 != False:
-            if fight_status == True:
-                fight_status = False
-                turn = 1
-                print('战斗结束')
-            choose_reward()
-        elif res5 != False:
-            finish()
-        elif res7 != False:
-            if choose_card_status == False:
-                print('选择主题卡包')
-                choose_card_status = True
-        else:
-            sleep(1000)
-
-# main = cv2.imread('./pic/main.png')
-# drive = cv2.imread('./pic/drive.png')
-# find_and_click(main, drive)
-
-# fight()
-# choose_reward()
+# 打印模块数
+def print_module():
+    module_pic = cv2.imread('pic/module.png')
+    template = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_BGR2RGB)
+    tup1, tup2 = find(template, module_pic)
+    module_num_pic = pyautogui.screenshot(region=[tup2[0], tup1[1], 100, tup2[1] - tup1[1]])
+    print('模块', pytesseract.image_to_string(module_num_pic, lang='chi_sim').replace('\n', ''))
